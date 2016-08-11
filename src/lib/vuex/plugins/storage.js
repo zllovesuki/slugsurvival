8ef558ce185e4382dd185b2eceb4a3ec0a2bbadf
Rachel Chen <me@rachelchen.me>
2016-08-11 14:23:38 -0700

    bugfix: auto driver selection for localForage

--- src/lib/vuex/plugins/storage.js
+++ src/lib/vuex/plugins/storage.js
@@ -1,7 +1,6 @@
 var localforage = require('localforage')
 
 module.exports = localforage.createInstance({
-    driver: localforage.INDEXEDDB,
     name: "events",
     version: 1.0,
     storeName: "kvStoreForCalendar"
