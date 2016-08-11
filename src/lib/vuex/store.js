b2e1b259a15c4201dd2a75008c40e36078a46436
Rachel Chen <me@rachelchen.me>
2016-08-11 12:53:33 -0700

    initial plugin for autosave

--- src/lib/vuex/store.js
+++ src/lib/vuex/store.js
@@ -1,11 +1,15 @@
 var	Vue = require('vue'),
-    Vuex = require('vuex');
+    Vuex = require('vuex'),
+    localforage = require('localforage');
 
 Vue.use(Vuex);
 
 var store = new Vuex.Store({
     state: require('./state'),
-    mutations: require('./mutations')
+    mutations: require('./mutations'),
+    plugins: [
+        require('./plugins/autosave')(storage)
+    ]
 })
 
 module.exports = store;
