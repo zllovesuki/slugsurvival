4d0bf52b39327e2c9badef6aaf082e2308395fd3
Rachel Chen <me@rachelchen.me>
2016-08-12 18:26:10 -0700

    don't replaceHash on change

--- src/lib/vuex/plugins/autosave.js
+++ src/lib/vuex/plugins/autosave.js
@@ -8,7 +8,7 @@ module.exports = function(storage) {
         store.subscribe(function(mutation, state) {
             if (listen.indexOf(mutation.type) !== -1) {
                 var termId = mutation.payload[0];
-                store.dispatch('replaceHash', termId)
+                //store.dispatch('replaceHash', termId)
                 storage.setItem(termId, state.events[termId])
             }
         })
