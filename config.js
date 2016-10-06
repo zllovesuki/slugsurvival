18d381e2d38f18f04cc34c0f92b020165d53532b
Rachel Chen <me@rachelchen.me>
2016-10-05 21:13:00 -0700

    stateless

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
     apiVersion: 2,
-    port: require('./config.json').port,
-    siteURL: require('./config.json').siteURL,
-    dbURL: require('./config.json').dbURL,
-    trackingURL: require('./config.json').trackingURL,
-    notifyURL: require('./config.json').notifyURL,
-    monitoredTerm: require('./config.json').monitoredTerm,
-    analytics: require('./config.json').analytics
+    port: 3001,
+    siteURL: require('./config/config.json').siteURL,
+    dbURL: require('./config/config.json').dbURL,
+    trackingURL: require('./config/config.json').trackingURL,
+    notifyURL: require('./config/config.json').notifyURL,
+    monitoredTerm: require('./config/config.json').monitoredTerm,
+    analytics: require('./config/config.json').analytics
 }
