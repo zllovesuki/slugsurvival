74159e4aff412d628e19b92c2d33d3d54a2b1032
Rachel Chen <me@rachelchen.me>
2016-10-11 23:00:11 -0700

    apparently span is not valid for clicking

--- src/app.vue
+++ src/app.vue
@@ -4,7 +4,7 @@
             <div class="clearfix mt3">
                 <div class="mb2 sm-flex center nowrap">
                     <div class="flex-auto block">
-                        <p class="inline h2 not-clickable">SlugSurvival | <small class="muted" >{{ title }}</small></p>
+                        <router-link style="text-decoration: none;" class="inline h2 clickable black" :to="{ name: 'mainPage' }">SlugSurvival | <small class="muted" >{{ title }}</small></router-link>
                     </div>
                 </div>
 
