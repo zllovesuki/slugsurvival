4135b340cc5cc940c4b1f53240f9c6f79a3a142f
Rachel Chen <me@rachelchen.me>
2016-10-31 20:50:54 -0700

    [WIP] high performance filter

--- src/views/planner/list.vue
+++ src/views/planner/list.vue
@@ -19,8 +19,9 @@
                         <option value="all">Subject...</option>
                         <option :value="code" v-for="(name, code) in subjectList">{{ name }}</option>
                     </select>
-                    <select class="inline-block col-4 field">
+                    <select class="inline-block col-4 field" v-model="filter.location">
                         <option value="all">Location...</option>
+                        <option :value="location" v-for="location in locations">{{ location }}</option>
                     </select>
                 </div>
             </div>
@@ -37,14 +38,15 @@
                         <option value="all">Subject...</option>
                         <option :value="code" v-for="(name, code) in subjectList">{{ name }}</option>
                     </select>
-                    <select class="inline-block col-4 field">
+                    <select class="inline-block col-4 field" v-model="filter.location">
                         <option value="all">Location...</option>
+                        <option :value="location" v-for="location in locations">{{ location }}</option>
                     </select>
                 </div>
             </div>
         </div>
         <hr class="mb2" />
-        <div class="bg-white rounded mb2" v-for="(subjectCourses, subject) in courses" v-if="ready" v-show="filter.subject == 'all' || filter.subject == subject">
+        <div class="bg-white rounded mb2" v-for="(subjectCourses, subject) in courses" track-by="subject" v-show="hideSubject[subject] !== true">
             <div class="m0 p1">
                 <div class="clearfix">
                     <span class="btn black h4">{{ subject }}</span>
@@ -68,7 +70,7 @@
                         Time and Location
                     </div>
                 </div>
-                <div class="h5 clearfix border clickable" @click="promptAddClass(course)" v-for="course in subjectCourses">
+                <div class="h5 clearfix border clickable" @click="promptAddClass(course)" v-for="course in subjectCourses" track-by="course.num" v-show="hideCourses[course.num] !== true">
                     <div class="p1 sm-col sm-col-2 overflow-hidden nowrap">
                         {{ course.c }} - {{ course.s }}
                     </div>
@@ -81,12 +83,12 @@
                     <div class="p1 sm-col sm-col-5 overflow-hidden nowrap bold">
                         {{
                             course.loct.map(function(el) {
-                                if (el.t === null) {
+                                if (el.t === false) {
                                     return 'Cancelled';
-                                }else if (el.t === false) {
-                                    return 'TBA';
+                                }else if (el.t === null) {
+                                     return 'TBA';
                                 }else{
-                                    return [el.t.day.map(long2short).join(', '), helper.tConvert(el.t.time.start) + '-' + helper.tConvert(el.t.time.end), el.loc].join(' / ')
+                                    return [el.t.day.map(long2short).join(', '), helper.tConvert(el.t.time.start) + '-' + helper.tConvert(el.t.time.end), el.loc === null ? 'TBA' : el.loc].join(' / ')
                                 }
                             }).join(', ')
                         }}
@@ -104,11 +106,15 @@ module.exports = {
         return {
             ready: false,
             courses: {},
+            locations: [],
             helper: helper,
             fixedToTop: false,
             filter: {
-                subject: 'all'
-            }
+                subject: 'all',
+                location: 'all'
+            },
+            hideSubject: {},
+            hideCourses: {}
         }
     },
     computed: {
@@ -123,7 +129,7 @@ module.exports = {
         },
         subjectList: function() {
             return this.$store.getters.subjectList;
-        }
+        },
     },
     methods: {
         long2short: function(el) {
@@ -173,6 +179,61 @@ module.exports = {
                 }
                 return alertHandle()
             })
+        },
+        getLocations: function() {
+            var flatCourses = this.$store.getters.flatCourses[this.termId];
+            return Object.keys(flatCourses).map(function(courseNum) {
+                return flatCourses[courseNum].loct.map(function(loct) {
+                    if (loct.t === false) {
+                        return 'Cancelled'
+                    }else if (loct.loc === null) {
+                        return 'TBA'
+                    }else {
+                        return loct.loc
+                    }
+                })
+            }).reduce(function(a, b) {
+                return a.concat(b);
+            }).filter(function(value, index, self) {
+                return self.indexOf(value) === index;
+            }).sort(helper.naturalSorter);
+        },
+        doFilter: function() {
+            var self = this;
+            for (var subject in this.courses) {
+                if (this.filter.subject == 'all') {
+                    self.hideSubject = {};
+                }else{
+                    if (subject != this.filter.subject) self.hideSubject[subject] = true
+                    else self.hideSubject[subject] = false
+                }
+            }
+            for (var subject in this.courses) {
+                if (this.filter.location == 'all') {
+                    self.hideCourses = {};
+                }else{
+                    this.courses[subject].map(function(course) {
+                        if (course.loct.filter(function(loct) {
+                            return (loct.t === false ? 'Cancelled' : loct.loc === null ? 'TBA' : loct.loc) == self.filter.location;
+                        }).length === 0) {
+                            self.hideCourses[course.num] = true
+                        }else{
+                            self.hideCourses[course.num] = false
+                        }
+                    })
+                }
+                if (this.courses[subject].reduce(function(total, course) {
+                    return self.hideCourses[course.num] === true ? total : total + 1;
+                }, 0) === 0) self.hideSubject[subject] = true
+            }
+        }
+    },
+    watch: {
+        'filter.subject': function() {
+            this.doFilter();
+        },
+        'filter.location': function() {
+            this.doFilter();
         }
     },
     mounted: function() {
@@ -180,6 +241,8 @@ module.exports = {
         var self = this;
         self.$store.dispatch('fetchTermCourses').then(function() {
             self.courses = self.$store.getters.sortedCourses[self.termId];
+            self.doFilter();
+            self.locations = self.getLocations();
             self.ready = true;
             $script.ready('bundle', function() {
                 self.$nextTick(function() {
