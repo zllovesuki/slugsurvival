08a0c687b96a938120b70852e0800eb8be27cb87
Rachel Chen <me@rachelchen.me>
2017-02-28 23:20:44 -0800

    type check

--- src/views/analytics/course.vue
+++ src/views/analytics/course.vue
@@ -309,6 +309,7 @@ module.exports = {
                 return res.json();
             }).then(function(res) {
                 if (res && res.ok && res.results && res.results.length > 0) self.heat = res.results.map(function(obj) {
+                    if (!self.flatCourses[self.termCode]) return;
                     if (!self.flatCourses[self.termCode][obj.group]) return;
                     return {
                         course: self.flatCourses[self.termCode][obj.group],
@@ -325,6 +326,7 @@ module.exports = {
                 return res.json();
             }).then(function(res) {
                 if (res && res.ok && res.results && res.results.length > 0) self.compacted = res.results.map(function(obj) {
+                    if (!self.flatCourses[self.termCode]) return;
                     if (!self.flatCourses[self.termCode][obj.group]) return;
                     return {
                         course: self.flatCourses[self.termCode][obj.group],
